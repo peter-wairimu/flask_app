eb36327a9b5cef6a7bd9446790222f19c0356fcb
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-21 13:42:35 +0300

    added post and get method

--- app/auth.py
+++ app/auth.py
@@ -4,7 +4,7 @@ from flask import Blueprint,render_template
 auth = Blueprint = Blueprint('auth',__name__)
 
 
-@auth.route('/login')
+@auth.route('/login',methods =['GET','POST'])
 def login():
     return render_template('auth/login.html')
 
@@ -12,7 +12,7 @@ def login():
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/signup')
+@auth.route('/signup',methods = ['GET','POST'])
 def signup():
     return render_template('auth/sign_up.html')
 
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint,render_template
 
 views = Blueprint('views',__name__)
 
-@views.route('/home')
+@views.route('/')
 def home():
     return render_template("index.html")
 
