119fb70377a296d76bec9bfe6ebf736d3650e897
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-21 13:55:56 +0300

    added variables

--- app/auth.py
+++ app/auth.py
@@ -6,8 +6,6 @@ auth = Blueprint = Blueprint('auth',__name__)
 
 @auth.route('/login',methods =['GET','POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template('auth/login.html')
 
 @auth.route('/logout')
@@ -16,6 +14,13 @@ def logout():
 
 @auth.route('/signup',methods = ['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+
     return render_template('auth/sign_up.html')
 
 
