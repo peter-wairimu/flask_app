4521102448e0c3b6ac97d88e281b859ed93e287a
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-21 10:27:47 +0300

    added index.html and modified the page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    return "<h1>Hello peter</h1>"
+    return render_template("index.html")
 
